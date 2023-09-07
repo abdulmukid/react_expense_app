118814a59fe091ebbb6eaf85706202cb33ab27a3
abdulmukid <abdulmukid40@gmail.com>
2023-09-07 01:14:17 +0100

    added props in order to remove hard coded values

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import ExpenseModule from "./components/ExpenseModule";
 
+const expenses = [
+  { id: "e1", title: "New TV", amount: 1000.0, date: new Date(2023, 2, 12) },
+  { id: "e2", title: "Shoes", amount: 100.0, date: new Date(2023, 5, 30) },
+  { id: "e3", title: "Toilet Paper", amount: 4.0, date: new Date(2023, 2, 2) },
+  { id: "e4", title: "Junk Food", amount: 30.0, date: new Date(2023, 7, 7) },
+];
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseModule></ExpenseModule>
+      <ExpenseModule
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseModule>
+      <ExpenseModule
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseModule>
+      <ExpenseModule
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseModule>
     </div>
   );
 }
--- src/components/ExpenseModule.js
+++ src/components/ExpenseModule.js
@@ -1,17 +1,21 @@
 import './ExpenseModule.css';
+// de-structor the incoming props paramter here to make the values more clear
+function ExpenseModule({date, title, amount}) {
 
-function ExpenseModule() {
-  const expenseDate = new Date(2022, 2, 23);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 1000;
-
+  const month = date.toLocaleString('en-US', {month: 'long'});
+  const day = date.toLocaleString('en-US', { day: '2-digit'});
+  const year = date.getFullYear();
 
   return (
     <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div className='expense-item__price'>{expenseAmount}</div>
+        <h2>{title}</h2>
+        <div className='expense-item__price'>{amount}</div>
       </div>
     </div>
   );
