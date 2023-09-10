37b332f1485ddd44dea2c39ff2d2afd862296264
abdulmukid <abdulmukid40@gmail.com>
2023-09-10 23:39:58 +0100

    prettify the code

--- src/components/expenses/ExpenseModule.js
+++ src/components/expenses/ExpenseModule.js
@@ -1,17 +1,17 @@
 import ExpenseCard from "../ui/ExpenseCard";
-import {useState} from 'react';
+import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import "../../styling/expenses/ExpenseModule.css";
 
 // destructor the incoming props paramter here to make the values more clear
 // destructered value names Must match what they are being passed in as
 const ExpenseModule = ({ date, expenseTitle, amount }) => {
-  const [title, setTitle] = useState(expenseTitle)
+  //useState is a hook that is used to trigger the React DOM to refresh the component
+  const [title, setTitle] = useState(expenseTitle);
 
-  const clickHandler= () => {
-    setTitle('Updated!')
-    console.log(title)
-  }
+  const clickHandler = () => {
+    setTitle("Updated!");
+  };
 
   return (
     <ExpenseCard className="expense-item">
@@ -23,6 +23,6 @@ const ExpenseModule = ({ date, expenseTitle, amount }) => {
       <button onClick={clickHandler}>click here</button>
     </ExpenseCard>
   );
-}
+};
 
 export default ExpenseModule;
