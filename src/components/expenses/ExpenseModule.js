09f28435a89fe5646a1d444ce4ac74800d4098b8
abdulmukid <abdulmukid40@gmail.com>
2023-09-10 15:42:03 +0100

    added useState hooks for updating title

--- src/components/expenses/ExpenseModule.js
+++ src/components/expenses/ExpenseModule.js
@@ -1,10 +1,18 @@
 import ExpenseCard from "../ui/ExpenseCard";
+import {useState} from 'react';
 import ExpenseDate from "./ExpenseDate";
 import "../../styling/expenses/ExpenseModule.css";
 
 // destructor the incoming props paramter here to make the values more clear
 // destructered value names Must match what they are being passed in as
-const ExpenseModule = ({ date, title, amount }) => {
+const ExpenseModule = ({ date, expenseTitle, amount }) => {
+  const [title, setTitle] = useState(expenseTitle)
+
+  const clickHandler= () => {
+    setTitle('Updated!')
+    console.log(title)
+  }
+
   return (
     <ExpenseCard className="expense-item">
       <ExpenseDate expenseDate={date} />
@@ -12,6 +20,7 @@ const ExpenseModule = ({ date, title, amount }) => {
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
       </div>
+      <button onClick={clickHandler}>click here</button>
     </ExpenseCard>
   );
 }
--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -6,22 +6,22 @@ const Expenses = ({ items }) => {
   return (
     <ExpenseCard className="expenses">
       <ExpenseModule
-        title={items[0].title}
+        expenseTitle={items[0].title}
         amount={items[0].amount}
         date={items[0].date}
       />
       <ExpenseModule
-        title={items[1].title}
+        expenseTitle={items[1].title}
         amount={items[1].amount}
         date={items[1].date}
       />
       <ExpenseModule
-        title={items[2].title}
+        expenseTitle={items[2].title}
         amount={items[2].amount}
         date={items[2].date}
       />
       <ExpenseModule
-        title={items[3].title}
+        expenseTitle={items[3].title}
         amount={items[3].amount}
         date={items[3].date}
       />
